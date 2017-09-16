69156cf657b8028a690f84b4d229b1937b22c3a3
seem-less <mgirach@gmail.com>
2017-09-16 14:41:29 +0700

    Update motor.js

--- Johnny-Five scripts/motor.js
+++ Johnny-Five scripts/motor.js
@@ -1,10 +1,12 @@
+//This scripts adds a motor object and let's us control that motor via the REPL.
+
 var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function(){
-   var motor = new five.Motor(3);
+   var motor = new five.Motor(3); //adding the motor object so that it can be controlled.
     
-    this.repl.inject({
+    this.repl.inject({ //including the motor object in the REPL so that it can be controlled directly via the CMD
         motor:motor
     });
 });
