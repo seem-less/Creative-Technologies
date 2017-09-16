62e639cf96c29b365d6d9b017931f9f65e0b856a
seem-less <mgirach@gmail.com>
2017-09-16 14:53:39 +0700

    Update temps.js

--- Johnny-Five scripts/temps.js
+++ Johnny-Five scripts/temps.js
@@ -1,11 +1,15 @@
+//This script is used to measure the temperature that is received through the temperature sensor
+// and enables us to graph it using the NPM package that was downloaded called 'barcli'
+
 var five = require("johnny-five");
 var board = new five.Board();
 
-var Barcli = require("barcli");
+var Barcli = require("barcli");  //including the 'barcli' package so that we can use it to graph the temperatures that are received
+                                 //through the sensor.
 
-var celsiusGraph = new Barcli({
-    label : 'Celsius',
-    range : [6, 50]
+var celsiusGraph = new Barcli({  //including the celsius graph
+    label : 'Celsius',  // Labelling the graph as "Celsius"      
+    range : [6, 50]     // setting the range so that the values of the room will appear on the graph.
 });
 
 var kelvinGraph = new Barcli({
@@ -14,13 +18,13 @@ var kelvinGraph = new Barcli({
 });
 
 board.on("ready", function(){
-    var temp = new five.Thermometer({
+    var temp = new five.Thermometer({ //Including the thermometer object from the Johnny-Five API
     controller: "TMP36",
     pin: "A0"
     });
     
-    temp.on("data", function(){
-    kelvinGraph.update(this.kelvin);
+    temp.on("data", function(){  //The 'data' event.  When data is received the functions in the 'data' event block will occur.
+    kelvinGraph.update(this.kelvin);  //"this.kelvin" is the data of temperature in Kelvin, coming into the board. "Update()" is to make the graph update with every new data retrieved.
     celsiusGraph.update(this.celsius);
     });
     
